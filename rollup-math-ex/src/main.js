52a28d74533c4e4b3f16d0108ab894d54a85b2af
klequis <klequis@gmail.com>
2019-03-01 19:30:07 -0800

    init

--- rollup-math-ex/src/main.js
+++ rollup-math-ex/src/main.js
@@ -1,8 +1,42 @@
-
+/**
+ * @name add
+ *
+ * @function
+ *
+ * @description
+ * Add two numbers and return the sum
+ * @param {number} a
+ * @param {number} b
+ *
+ * @returns {number}
+ *
+ * @example
+ * add(1,3)
+ * // => 4
+ *
+ */
 
 const add = (a, b) => {
   return a + b
 }
+
+/**
+ * @name subtract
+ *
+ * @function
+ *
+ * @description
+ * Subtract two numbers and return the difference
+ * @param {number} a
+ * @param {number} b
+ *
+ * @returns {number}
+ *
+ * @example
+ * subtract(4,2)
+ * // => 2
+ *
+ */
 
 const subtract = (a, b) => {
   return a - b
